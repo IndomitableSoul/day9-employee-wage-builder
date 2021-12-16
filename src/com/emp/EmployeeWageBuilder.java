2efb4b5ac96d0a82f8fd6a1fa3ee15200f1f4640
nikita <amarnikita09@gmail.com>
2021-12-16 13:08:31 +0530

    Refactored EmployeeWageBuilder.java to get emp wage for different companies as per UC8

--- src/com/emp/EmployeeWageBuilder.java
+++ src/com/emp/EmployeeWageBuilder.java
@@ -4,18 +4,17 @@ public class EmployeeWageBuilder {
 	//constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	private static final int MAX_HRS_IN_MONTH = 10;
 	
-	public static int computeEmpWage() {
+	
+	public static int computeEmpWage(String company, int empRate, 
+									int numOfDays, int maxHrs) {
 		  //variables
 		  int totalEmpHrs = 0, totalEmpWage = 0;
 		  int totalWorkingDays = 0;
 				
 		  //Computation
-		  while (totalEmpHrs<=MAX_HRS_IN_MONTH && 
-				  totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		  while (totalEmpHrs<=maxHrs && 
+				  totalWorkingDays < numOfDays) {
 					
 		     int empHrs = 0;
 		     totalWorkingDays++;
@@ -33,7 +32,7 @@ public class EmployeeWageBuilder {
 					}
 					
 		     totalEmpHrs += empHrs;
-		     int empWage = empHrs * EMP_RATE_PER_HOUR;
+		     int empWage = empHrs * empRate;
 		     totalEmpWage += empWage;		
 		}
 				
@@ -41,7 +40,13 @@ public class EmployeeWageBuilder {
 				return totalEmpWage;
 	 }
 	 public static void main(String[] args) {
-		computeEmpWage();
+			/* UC-8
+			 * Compute Employee Wage for multiple companies - Note: Each Company has its own
+			 * wage, number of working days and working hours per month - Use Class Method
+			 * with function parameters instead of Class Variables
+			 */
+		computeEmpWage("DMart", 20, 2, 10);
+		computeEmpWage("Reliance Retail",10, 4,20);
 	}
 	
 }
